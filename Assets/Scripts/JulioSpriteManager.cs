3311455d8d6ddb6cf6b18e27a8ea77c94c109228
Nathan <nathan.pm96@gmail.com>
2025-11-16 11:44:08 +0100

    Julio image prefab

--- Assets/Scripts/JulioSpriteManager.cs
+++ Assets/Scripts/JulioSpriteManager.cs
@@ -5,47 +5,48 @@ using UnityEngine.UI;
 
 public class JulioSpriteManager : MonoBehaviour
 {
-    [SerializeField]
-    private Image _julioImage;
+	[SerializeField]
+	private Image _julioImage;
 
-    [SerializeField]
-    private GameObject _accesoriesImagePrefab;
+	[SerializeField]
+	private GameObject _accesoriesImagePrefab;
 
-    [SerializeField]
-    private Transform _accesoriesReferencePoint;
+	[SerializeField]
+	private Transform _accesoriesReferencePoint;
 
-    [SerializeField]
-    private JulioSpritesScriptable _julioSprites;
-    [SerializeField]
-    private AccesoriesScriptable[] _accesories;
+	[SerializeField]
+	private JulioSpritesScriptable _julioSprites;
+	[SerializeField]
+	private AccesoriesScriptable[] _accesories;
 
-    [SerializeField]
-    private bool isThisNarrativa;
+	[SerializeField]
+	private bool isThisNarrativa;
 
-    void Start()
-    {
-        if (isThisNarrativa)
-        {
-            _julioImage.sprite = _julioSprites.narrativeSprites[Player.combatRound];
+	void Start()
+	{
+		if (isThisNarrativa)
+		{
+			_julioImage.sprite = _julioSprites.narrativeSprites[Player.combatRound];
 
-            var accesoriesDictionary = _accesories.ToDictionary(x => x.accesory);
+			var accesoriesDictionary = _accesories.ToDictionary(x => x.accesory);
+			Player.Accesories = new List<Accesories>{ Accesories.Camisa, Accesories.Chaleco};
 			for (int i = 0; i < Player.Accesories.Count; i++)
 			{
-                Image newImage = Instantiate(_accesoriesImagePrefab, _accesoriesReferencePoint).GetComponent<Image>();
-                newImage.sprite = accesoriesDictionary[Player.Accesories[i]].spriteFront;
-            }
-        }
-    }
+				Image newImage = Instantiate(_accesoriesImagePrefab, _accesoriesReferencePoint).GetComponent<Image>();
+				newImage.sprite = accesoriesDictionary[Player.Accesories[i]].spriteFront;
+			}
+		}
+	}
 
-    public void UpdateCombatSprites()
-    {
-        _julioImage.sprite = _julioSprites.combatSprite;
+	public void UpdateCombatSprites()
+	{
+		_julioImage.sprite = _julioSprites.combatSprite;
 
-        var accesoriesDictionary = _accesories.ToDictionary(x => x.accesory);
-        for (int i = 0; i < Player.Accesories.Count; i++)
-        {
-            Image newImage = Instantiate(_accesoriesImagePrefab, _accesoriesReferencePoint).GetComponent<Image>();
-            newImage.sprite = accesoriesDictionary[Player.Accesories[i]].spriteBack;
-        }
-    }
+		var accesoriesDictionary = _accesories.ToDictionary(x => x.accesory);
+		for (int i = 0; i < Player.Accesories.Count; i++)
+		{
+			Image newImage = Instantiate(_accesoriesImagePrefab, _accesoriesReferencePoint).GetComponent<Image>();
+			newImage.sprite = accesoriesDictionary[Player.Accesories[i]].spriteBack;
+		}
+	}
 }
